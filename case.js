94eb855b311282ff0b2f04769ca260aa1e2396f5
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-12 03:36:25 +0600

    string problem-practice.js files

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = "blackPink";
+const userInput = "blackPinK"
+console.log(userName.toUpperCase());
+console.log(userInput.toUpperCase());
+if( userName.toUpperCase() === userInput.toUpperCase()){
+    console.log("valid")
+}
+else {
+    console.log("invalid hobe")
+}
