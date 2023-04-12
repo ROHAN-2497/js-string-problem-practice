7c03da1e84618496838af34e6e011f5f2998c98b
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-13 03:26:44 +0600

    max.js files

---
+++ max.js
@@ -0,0 +1,22 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+// if(jim > dela){
+//     console.log("jim will get the cake")
+// }
+// else if (dela > chinku){
+//     console.log("dela will get the cake")
+// }
+// else{
+//     console.log("chinku will get the cake")
+// }
+
+if (jim > dela && jim > chinku){
+    console.log("jim will be get the cake")
+}
+else if( dela >jim && dela > chinku){
+    console.log("dela  will get the cake")
+}
+else{
+    console.log("chinku will get the cake")
+}
