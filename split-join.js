5793d56d941f021a772077ced0b1b34983202877
rohan future devoloper <xrohaislam373@gmail.com>
2023-04-12 15:56:19 +0600

    split.js files

---
+++ split-join.js
@@ -0,0 +1,5 @@
+const sentences = 'i am good boy i want a next level. web developer and i wants. islamic wife '
+const parts  = sentences.split(' ');
+const sentence = sentences.split('.');
+const chars = sentences.split('')
+console.log(chars);
